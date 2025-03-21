82590cabb56abc7a2de5512e52234d0c55b36850
Teodora Berca <115499011+teodora-berca@users.noreply.github.com>
2025-03-21 09:07:59 +0000

    teste bilete - commit1

---
+++ src/test/java/ro/unibuc/hello/service/TicketsServiceTest.java
@@ -0,0 +1,58 @@
+package ro.unibuc.hello.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import ro.unibuc.hello.data.TicketEntity;
+import ro.unibuc.hello.data.TicketRepository;
+import ro.unibuc.hello.dto.Ticket;
+
+import ro.unibuc.hello.exception.EntityNotFoundException;
+
+import java.beans.Transient;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+public class TicketsTest {
+
+    @Mock
+    private TicketRepository ticketRepository;
+
+    @InjectMocks
+    private TicketsService ticketsService = new TicketsService();
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    //METODE
+    //save ticket
+    //get ticket by id
+    //get all tickets
+    //delete ticket
+
+
+    @Test 
+    void testSaveTicket(){
+
+    }
+
+    @Test testDeleteTicket(){
+
+    }
+
+    @
+
+
+    
+}
