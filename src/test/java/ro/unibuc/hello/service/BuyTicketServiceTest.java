c7f3c2366abd0ec579de76f14586286987bba412
Teodora Berca <115499011+teodora-berca@users.noreply.github.com>
2025-03-25 22:35:27 +0000

    Teste pentru Buy Ticket Service

--- src/test/java/ro/unibuc/hello/service/BuyTicketServiceTest.java
+++ src/test/java/ro/unibuc/hello/service/BuyTicketServiceTest.java
@@ -99,76 +99,297 @@ public class BuyTicketServiceTest {
         assertThrows(EntityNotFoundException.class, () -> buyTicketsService.buyTicket(eventId, userId));
     }
 
-    // @Test 
-    // void testBuyTicketWithPriceInFirstRange() {
-    //     String eventId = "12345";
-    //     EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
-    //                                             100, 0, 45, "100", "operatie");
-    //     String userId = "3";
-    //     UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
-    //                                         "0725661234", 0);
+    @Test 
+    void testBuyTicketWithDiscountedPriceWhenEventIdIsWrong() {
+        String eventId = "idGresit";
+        String userId = "3";
+        int discount = 20;
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 0);
+        when(eventRepository.findById(eventId)).thenReturn(Optional.empty());
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        assertThrows(EntityNotFoundException.class, () -> buyTicketsService.buyTicketWithDiscountedPrice(eventId, userId, discount));
+    }
 
-    //     when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
-    //     when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+    @Test 
+    void testBuyTicketWithDiscountedPriceWhenUserIdIsWrong() {
+        String eventId = "12345";
+        String userId = "idGresit";
+        int discount = 20;
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 250, "100", "operatie");
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+        assertThrows(EntityNotFoundException.class, () -> buyTicketsService.buyTicketWithDiscountedPrice(eventId, userId, discount));
+    }
 
-    //     List<Ticket> tickets = new ArrayList<>();
-    //     when(ticketsService.getAllTickets()).thenReturn(tickets);
-
-    //     doNothing().when(eventsService).checkSales(any(EventEntity.class));
-
-    //     when(eventsService.updateEvent(anyString(), any(EventEntity.class)).thenReturn();
-    //     when(usersService.updateUser(anyString(), any(UserEntity.class));
-    //     doNothing().when(ticketRepository).save(any(TicketEntity.class));
-
-    //     buyTicketsService.buyTicket(eventId, userId);
-    //     assertEquals(1, event.getSoldTickets());
-    //     assertEquals(2, user.getPoints());
-    // }
-
-    void testBuyTicketWithPriceInFirstRange() {
+    @Test 
+    void testBuyTicketWithDiscountedPriceWhenOutOfTickets() {
         String eventId = "12345";
         EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
-                                                100, 10, 45, "100", "operatie");
+                                                100, 100, 250, "100", "operatie");
         String userId = "3";
         UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
                                             "0725661234", 0);
-
-        List<Ticket> existingTickets = List.of(new Ticket("0", eventId, userId, 1, 1, 2024, 50));
-
+        int discount = 20;
         when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
         when(userRepository.findById(userId)).thenReturn(Optional.of(user));
-        when(ticketsService.getAllTickets()).thenReturn(existingTickets);
-        when(ticketRepository.save(any(TicketEntity.class))).thenAnswer(invocation -> invocation.getArgument(0));
-
-        buyTicketsService.buyTicket(eventId, userId);
-
-        assertEquals(11, event.getSoldTickets()); 
-        assertEquals(2, user.getPoints());
-
-        verify(ticketRepository, times(1)).save(any(TicketEntity.class));
-        verify(eventsService, times(1)).updateEvent(eq(eventId), any(EventEntity.class));
-        verify(usersService, times(1)).updateUser(eq(userId), any(UserEntity.class));
+        assertThrows(EntityNotFoundException.class, () -> buyTicketsService.buyTicketWithDiscountedPrice(eventId, userId, discount));
     }
 
-    // @Test 
-    // void testBuyTicketWithPriceInSecondRange() {
+    @Test 
+    void testBuyTicketWithDiscountedPrice() {
+        String eventId = "12345";
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 250, "100", "operatie");
+        String userId = "3";
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 0);
+        Ticket ticket1 = new Ticket("1", "3", "12345", 22, 3, 2025, 250);
+        Ticket ticket2 = new Ticket("2", "4", "12345", 22, 3, 2025, 250);
+        Ticket ticket3 = new Ticket("3", "5", "12345", 22, 3, 2025, 250);
+        List<Ticket> tickets = List.of(ticket1, ticket2, ticket3);
+        when(ticketsService.getAllTickets()).thenReturn(tickets);
+        int discount = 20;
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        buyTicketsService.buyTicketWithDiscountedPrice(eventId, userId, discount);
+        assertEquals(1, event.getSoldTickets());
+        verify(eventRepository, times(1)).findById(eventId); 
+        verify(userRepository, times(1)).findById(userId);
+        verify(ticketRepository, times(1)).save(any(TicketEntity.class));
+    }
 
-    // }
+    @Test 
+    void testBuyTicketWithPriceInFirstRange() {
+        String eventId = "12345";
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 45, "100", "operatie");
+        String userId = "3";
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 0);
+        Ticket ticket1 = new Ticket("1", "3", "12345", 22, 3, 2025, 250);
+        Ticket ticket2 = new Ticket("2", "4", "12345", 22, 3, 2025, 250);
+        Ticket ticket3 = new Ticket("3", "5", "12345", 22, 3, 2025, 250);
+        List<Ticket> tickets = List.of(ticket1, ticket2, ticket3);
+        when(ticketsService.getAllTickets()).thenReturn(tickets);
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        buyTicketsService.buyTicket(eventId, userId);
+        assertEquals(1, event.getSoldTickets());
+        verify(eventRepository, times(1)).findById(eventId); 
+        verify(userRepository, times(1)).findById(userId);
+        verify(ticketRepository, times(1)).save(any(TicketEntity.class));
+        assertEquals(2, user.getPoints());
+    }
 
-    // @Test 
-    // void testBuyTicketWithPriceInThirdRange() {
+    @Test 
+    void testBuyTicketWithPriceInSecondRange() {
+        String eventId = "12345";
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 60, "100", "operatie");
+        String userId = "3";
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 0);
+        Ticket ticket1 = new Ticket("1", "3", "12345", 22, 3, 2025, 250);
+        Ticket ticket2 = new Ticket("2", "4", "12345", 22, 3, 2025, 250);
+        Ticket ticket3 = new Ticket("3", "5", "12345", 22, 3, 2025, 250);
+        List<Ticket> tickets = List.of(ticket1, ticket2, ticket3);
+        when(ticketsService.getAllTickets()).thenReturn(tickets);
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        buyTicketsService.buyTicket(eventId, userId);
+        assertEquals(1, event.getSoldTickets());
+        verify(eventRepository, times(1)).findById(eventId); 
+        verify(userRepository, times(1)).findById(userId);
+        verify(ticketRepository, times(1)).save(any(TicketEntity.class));
+        assertEquals(5, user.getPoints());
+    }
 
-    // }
+    @Test 
+    void testBuyTicketWithPriceInThirdRange() {
+        String eventId = "12345";
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 120, "100", "operatie");
+        String userId = "3";
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 0);
+        Ticket ticket1 = new Ticket("1", "3", "12345", 22, 3, 2025, 250);
+        Ticket ticket2 = new Ticket("2", "4", "12345", 22, 3, 2025, 250);
+        Ticket ticket3 = new Ticket("3", "5", "12345", 22, 3, 2025, 250);
+        List<Ticket> tickets = List.of(ticket1, ticket2, ticket3);
+        when(ticketsService.getAllTickets()).thenReturn(tickets);
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        buyTicketsService.buyTicket(eventId, userId);
+        assertEquals(1, event.getSoldTickets());
+        verify(eventRepository, times(1)).findById(eventId); 
+        verify(userRepository, times(1)).findById(userId);
+        verify(ticketRepository, times(1)).save(any(TicketEntity.class));
+        assertEquals(10, user.getPoints());
+    }
 
-    // @Test 
-    // void testBuyTicketWithPriceInFourthRange() {
+    @Test 
+    void testBuyTicketWithPriceInFourthRange() {
+        String eventId = "12345";
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 160, "100", "operatie");
+        String userId = "3";
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 0);
+        Ticket ticket1 = new Ticket("1", "3", "12345", 22, 3, 2025, 250);
+        Ticket ticket2 = new Ticket("2", "4", "12345", 22, 3, 2025, 250);
+        Ticket ticket3 = new Ticket("3", "5", "12345", 22, 3, 2025, 250);
+        List<Ticket> tickets = List.of(ticket1, ticket2, ticket3);
+        when(ticketsService.getAllTickets()).thenReturn(tickets);
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        buyTicketsService.buyTicket(eventId, userId);
+        assertEquals(1, event.getSoldTickets());
+        verify(eventRepository, times(1)).findById(eventId); 
+        verify(userRepository, times(1)).findById(userId);
+        verify(ticketRepository, times(1)).save(any(TicketEntity.class));
+        assertEquals(15, user.getPoints());
+    }
 
-    // }
+    @Test 
+    void testBuyTicketWithPriceInFifthRange() {
+        String eventId = "12345";
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 250, "100", "operatie");
+        String userId = "3";
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 0);
+        Ticket ticket1 = new Ticket("1", "3", "12345", 22, 3, 2025, 250);
+        Ticket ticket2 = new Ticket("2", "4", "12345", 22, 3, 2025, 250);
+        Ticket ticket3 = new Ticket("3", "5", "12345", 22, 3, 2025, 250);
+        List<Ticket> tickets = List.of(ticket1, ticket2, ticket3);
+        when(ticketsService.getAllTickets()).thenReturn(tickets);
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        buyTicketsService.buyTicket(eventId, userId);
+        assertEquals(1, event.getSoldTickets());
+        verify(eventRepository, times(1)).findById(eventId); 
+        verify(userRepository, times(1)).findById(userId);
+        verify(ticketRepository, times(1)).save(any(TicketEntity.class));
+        assertEquals(20, user.getPoints());
+    }
 
-    // @Test 
-    // void testBuyTicketWithPriceInFifthRange() {
+    @Test 
+    void testBuyTicketWithDiscountWhenDiscountIsNotAvailableForUser() {
+        String eventId = "12345";
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 250, "100", "operatie");
+        String userId = "3";
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 0);
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        int discount = 20;
 
-    // }
+        assertThrows(EntityNotFoundException.class, () -> buyTicketsService.buyTicketWithDiscount(eventId, userId, discount));
+    }
 
-    
+    @Test 
+    void testBuyTicketWithDiscountWhenDiscountIsNotAvailable() {
+        String eventId = "12345";
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 250, "100", "operatie");
+        String userId = "3";
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 0);
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        int discount = 70;
+        assertThrows(EntityNotFoundException.class, () -> buyTicketsService.buyTicketWithDiscount(eventId, userId, discount));
+    }
+
+    @Test 
+    void testBuyTicketWithDiscount20() {
+        String eventId = "12345";
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 250, "100", "operatie");
+        String userId = "3";
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 60);
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        int discount = 20;
+        buyTicketsService.buyTicketWithDiscount(eventId, userId, discount);
+        assertEquals(1, event.getSoldTickets());
+        verify(usersService, times(1)).updateUser(userId, user);
+        assertEquals(10, user.getPoints());
+    }
+
+    @Test 
+    void testBuyTicketWithDiscount50() {
+        String eventId = "12345";
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 250, "100", "operatie");
+        String userId = "3";
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 110);
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        int discount = 50;
+        buyTicketsService.buyTicketWithDiscount(eventId, userId, discount);
+        assertEquals(1, event.getSoldTickets());
+        verify(usersService, times(1)).updateUser(userId, user);
+        assertEquals(10, user.getPoints());
+    }
+
+    @Test 
+    void testBuyTicketWithDiscount100() {
+        String eventId = "12345";
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 250, "100", "operatie");
+        String userId = "3";
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 210);
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        int discount = 100;
+        buyTicketsService.buyTicketWithDiscount(eventId, userId, discount);
+        assertEquals(1, event.getSoldTickets());
+        verify(usersService, times(1)).updateUser(userId, user);
+        assertEquals(10, user.getPoints());
+    }
+
+    @Test 
+    void testBuyTicketWithDiscountWhenEventIdIsWrong() {
+        String eventId = "idGresit";
+        String userId = "3";
+        int discount = 20;
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 0);
+        when(eventRepository.findById(eventId)).thenReturn(Optional.empty());
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        assertThrows(EntityNotFoundException.class, () -> buyTicketsService.buyTicketWithDiscount(eventId, userId, discount));
+    }
+
+    @Test 
+    void testBuyTicketWithDiscountWhenUserIdIsWrong() {
+        String eventId = "12345";
+        String userId = "idGresit";
+        int discount = 20;
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 0, 250, "100", "operatie");
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+        assertThrows(EntityNotFoundException.class, () -> buyTicketsService.buyTicketWithDiscount(eventId, userId, discount));
+    }
+
+    @Test 
+    void testBuyTicketWithDiscountWhenOutOfTickets() {
+        String eventId = "12345";
+        EventEntity event = new EventEntity("12345", "eveniment", "descriere", "locatie", LocalDate.parse("2025-03-30"), "14:00",
+                                                100, 100, 250, "100", "operatie");
+        String userId = "3";
+        UserEntity user = new UserEntity("3", "Alexandra", 26, "Alexandra@email.com", "parola", 
+                                            "0725661234", 0);
+        int discount = 20;
+        when(eventRepository.findById(eventId)).thenReturn(Optional.of(event));
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        assertThrows(EntityNotFoundException.class, () -> buyTicketsService.buyTicketWithDiscount(eventId, userId, discount));
+    }
 }
